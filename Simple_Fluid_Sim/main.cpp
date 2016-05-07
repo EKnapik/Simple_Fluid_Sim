b2856e70e38ad43f8d1d0141e20e83745d0ba95c
Eric Knapik <eric11mk@gmail.com>
2016-05-07 12:27:08 -0400

    Initial Commit

---
+++ Simple_Fluid_Sim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simple_Fluid_Sim
+//
+//  Created by Eric Knapik on 5/7/16.
+//  Copyright © 2016 EKnapik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

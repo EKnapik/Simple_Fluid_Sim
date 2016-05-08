4226705940c05c7279ac36480ebef6f599a39b08
Eric Knapik <eric11mk@gmail.com>
2016-05-07 23:58:30 -0400

    Initial OpenGL boiler plate setup

--- Simple_Fluid_Sim/main.cpp
+++ Simple_Fluid_Sim/main.cpp
@@ -6,13 +6,66 @@
 //  Copyright © 2016 EKnapik. All rights reserved.
 //
 
+#include <GLUT/GLUT.h>
+#include <OpenGL/gl.h>
 #include <iostream>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+
+// 16 x 9
+#define WINDOW_HEIGHT 450
+#define WINDOW_WIDTH 800
+
+void initOpenGL(void);
+void render(void);
+
+int main(int argc, char * argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("Simple Fluid Simulation");
+    glutDisplayFunc(render);
+    initOpenGL();
+    
+    glutMainLoop();
+    return 0; // Cause GLUT allows us to even get back here...... (it doesn't)
 }
+
+
+
+void initOpenGL(void) {
+    glEnable(GL_DEPTH_TEST | GL_CULL_FACE | GL_BLEND);
+    glEnable(GL_TEXTURE_2D);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glClearColor( 0.0, 0.0, 0.0, 1.0 );
+    glCullFace(GL_FRONT);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    
+    return;
+}
+
+
+void render(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // poll events
+    // bind and draw
+    clock_t t;
+    t = clock();
+    float renderTime;
+    
+    // DO SOMETHING
+    t = clock() - t;
+    renderTime = t;
+    
+
+    
+    printf("Render took (%.4f seconds)\n",((float)renderTime)/CLOCKS_PER_SEC);
+    glutSwapBuffers();
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#!#!#!#!#!#!
+    // glutPostRedisplay();
+}
+
 
 
 /*
